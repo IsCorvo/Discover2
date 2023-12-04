fa1602a1d3319f943d989053168978e660dbb106
OCorvo <104469048+IsCorvo@users.noreply.github.com>
2023-12-04 21:40:44 +0000

    Commit de legenda

--- script.js
+++ script.js
@@ -16,8 +16,16 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     // Se tiver light mode, adcionar a imagem light
     img.setAttribute('src', './assets/avatar-light-mode.png')
+    img.setAttribute(
+      "alt",
+      "Foto de Calebe Medeiros, usando óculos, com uma blusa preta!"
+    )
   } else {
     // Se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Calebe Medeiros, usando óculos, blusa preta, mochila e máscara preta, como cabelo voando ao vento!."
+    )
   }
 }
